a3563a0f5ed35eaccbc7eae461cba4c8975ebefa
Oleg <oleg_09-95@mail.ru>
2022-01-17 15:26:07 +0300

    Lesson 17 Дополнение: ДА ошибка с версиями спринга, при новых версии сыпятся Error

--- src/main/java/ru/oleg/controllers/FirstController.java
+++ src/main/java/ru/oleg/controllers/FirstController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/first")
 public class FirstController {
 
+
     @GetMapping("/hello")
     public String helloPage() {
         return "first/hello";
