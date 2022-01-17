b4561dedec34536bd1658557c7fda2f31021352f
Oleg <oleg_09-95@mail.ru>
2022-01-17 16:31:54 +0300

    Lesson 19 Параметры GET запроса. Аннотация @RequestParam Если используется эта аннотация без параметра "required = false", то эти параметры в запросе будут обязательны, если их указать, то нет. HttpServletRequest - при этом способе извлекаются все параметры GET запроса

--- src/main/java/ru/oleg/controllers/FirstController.java
+++ src/main/java/ru/oleg/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.oleg.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Neil Alishev
@@ -13,7 +14,10 @@ public class FirstController {
 
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello my dear " + name + " " + surname);
         return "first/hello";
     }
 
